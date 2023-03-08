26a9ffd261e63d81afa74865173d137922703779
Anders <38501857+anderama100@users.noreply.github.com>
2023-03-08 13:46:23 -0700

    Update spirits.js

--- src/routes/spirits.js
+++ src/routes/spirits.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-//const Review = require("../controllers/reviews.js");
+const Review = require("../controllers/reviews.js");
 
 // Controller
 const {
